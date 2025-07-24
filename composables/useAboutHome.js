6624b6b299a4255cfd84d533f40c5c0131d26cbf
Gloriaza Odyardy <199865373+gloriazaodyardy@users.noreply.github.com>
2025-07-25 01:00:01 +0700

    Fix: incorrect query in useAboutHome.js

--- composables/useAboutHome.js
+++ composables/useAboutHome.js
@@ -4,30 +4,6 @@ export const useAboutHome = async () => {
     const query = `*[_type == "about"][0] {
         aboutImage,
         aboutDescription,
-        experience[]{
-            title,
-            company,
-            description,
-            startDate,
-            endDate,
-            isCurrent,
-        },
-        education[]{
-            title,
-            institution,
-            description,
-            startDate,
-            endDate,
-            isCurrent,
-        },
-        disciplines[]{
-            name
-        },
-        tools[]{
-            name,
-            icon,
-            
-        }
     }`;
 
     const data = await fetchSanity(query);
