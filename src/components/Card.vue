569b7bf4abe88e19b78ff518c28e5e67959d6597
Gloriaza Odyardy <gloriazalawrance@gmail.com>
2025-07-27 00:06:13 +0700

    create & develop Work page, fix styling card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,24 +18,22 @@ const props = defineProps({
 
 <template>
     <router-link :to="{ name: 'portfolio-detail', params: { id: portfolioId } }"
-        class="block rounded-lg border overflow-hidden relative group">
-        <div class="relative h-fit">
-            <div class="relative h-64 overflow-hidden w-full md:w-[30rem]">
-                <img :src="image" :alt="title"
-                    class="p-2 rounded-2xl w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
-            </div>
-            <div class="p-6 bg-white">
-                <h3 class="text-lg font-semibold mb-2 text-gray-900">
-                    {{ title }}
-                </h3>
-                <p class="text-gray-600 text-sm leading-relaxed line-clamp-3">
-                    {{ summary }}
-                </p>
-            </div>
-            <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
-                :class="overlayClass">
-                <span class="text-white text-2xl font-semibold">See more</span>
-            </div>
+        class="grid grid-rows-[auto_1fr] rounded-lg border overflow-hidden relative group">
+        <div class="relative h-64 overflow-hidden w-full">
+            <img :src="image" :alt="title"
+                class="p-2 rounded-2xl w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
+        </div>
+        <div class="p-6 bg-white flex flex-col">
+            <h3 class="text-lg font-semibold mb-2 text-gray-900 line-clamp-2 h-14">
+                {{ title }}
+            </h3>
+            <p class="text-gray-600 text-sm leading-relaxed line-clamp-1 mt-auto">
+                {{ summary }}
+            </p>
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
+            :class="overlayClass">
+            <span class="text-white text-2xl font-semibold">See more</span>
         </div>
     </router-link>
 </template>
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,1 +1,29 @@
-<template></template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import Card from '../components/Card.vue'
+import { useWork } from '../../composables/useWork'
+
+const workCards = ref([])
+
+onMounted(async () => {
+    workCards.value = await useWork()
+})
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen w-full px-4 pb-12">
+        <div class="w-full max-w-7xl mx-auto">
+            <h2 class="text-[#C35B3F] text-5xl md:text-7xl py-12">My Work</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-0 justify-items-center lg:[&>*:nth-child(3n-1)]:pt-12">
+                <template v-for="item in workCards" :key="item.slug?.current">
+                    <div v-if="item.slug && item.slug.current">
+                        <Card :title="item.title" :summary="item.summary"
+                            :image="item.imageUrl" :portfolio-id="item.slug.current"
+                            overlay-class="backdrop-blur-md bg-opacity-80" class="max-w-80">
+                        </Card>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
