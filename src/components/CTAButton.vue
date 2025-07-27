cb3c17c103dc794f88597bc301103b601e88642c
Gloriaza Odyardy <gloriazalawrance@gmail.com>
2025-07-27 14:15:31 +0700

    create and develop "Talk to Her!" page, modify footer & CTA button

---
+++ src/components/CTAButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const props = defineProps({
+    name: String,
+})
+
+</script>
+
+<template>
+    <button
+        class="w-50 md:w-66 py-4 md:py-6 text-lg md:text-2xl text-white rounded-md bg-linear-to-br from-[#C35B3F] from-30% to-[#D782BA] to-90% hover:opacity-80">{{ name }}</button>
+</template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,1 +1,16 @@
-<template></template>
+<script setup>
+import CTAButton from '@/components/CTAButton.vue';
+</script>
+
+<template>
+    <div class="w-full min-h-screen p-8 grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+        <div class="flex flex-col gap-8">
+            <p class="text-6xl md:text-7xl">Let's Keep</p>
+            <p class="text-6xl md:text-8xl pl-0 md:pl-12 font-[LibreBodoni] italic">in Touch</p>
+        </div>
+        <div class="flex flex-col gap-12">
+            <p class="text-2xl text-center md:text-start text-wrap">Whether you've got a question, a project idea, or just want to say hello, drop me a line and I'll be back in your inbox soon.</p>
+            <CTAButton name="Send Email" />
+        </div>
+    </div>
+</template>
