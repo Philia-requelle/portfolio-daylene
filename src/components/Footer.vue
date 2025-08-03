57a3afc695ad1a22460d6238a62fe03c64eb5903
Gloriaza Odyardy <199865373+gloriazaodyardy@users.noreply.github.com>
2025-08-04 00:00:48 +0700

    improve font path, decoration position, styling (app, homeview & components)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,33 +1,29 @@
 <script setup lang="ts">
 import CTAButton from './CTAButton.vue';
-
 </script>
-
 <template>
-
     <div
-        class="flex flex-col items-center px-0 md:px-15 lg:px-30 md:items-start gap-8 md:gap-20 text-center md:text-left">
+        class="flex flex-col items-center px-15 md:items-start gap-8 md:gap-20 text-center md:text-left">
         <div>
-            <p class="text-[2rem] md:text-7xl font-[LibreBodoni] italic">Have a Project in Mind?</p>
-            <p class="text-wrap md:text-5xl"><span class="text-orange-500">I'm all ears-big or small,</span> let's chat
-                <span class="text-orange-500">about how
-                    we can work together.</span>
+            <p class="text-5xl md:text-7xl font-[LibreBodoni] italic">Have a Project in Mind?</p>
+            <p class="text-wrap text-xl/8 md:text-5xl/14">
+                <span class="text-terracotta">I'm all ears-bi<span class="font-[LibreBodoni] text-2xl md:text-6xl">g</span> or small,</span> let's chat
+                <span class="text-terracotta">about how we can work to<span class="font-[LibreBodoni] text-2xl md:text-6xl">g</span>ether.</span>
             </p>
         </div>
         <CTAButton name="Get in Touch" />
-        <img src="../assets/arrow-downLeft.svg" alt="Arrow down-left"
-            class="absolute md:left-124 md:bottom-80 lg:left-140 lg:bottom-68 invisible md:visible" />
+        <img src="../assets/arrow-downLeft.svg" alt="Arrow down-left" aria-hidden="true"
+            class="absolute md:left-124 md:bottom-80 lg:left-100 lg:bottom-62 invisible md:visible" />
     </div>
     <div class="w-full text-center text-xs md:text-[1.5625rem]">
-        <div class="text-white bg-conic-90 from-[#A1BDB8] via-[#C35B3F] to-[#D782BA] py-8">
-            <div class="flex flex-col lg:flex-row lg:justify-around gap-4 lg:gap-0">
-                <div class="flex flex-col sm:flex-row justify-center gap-4 lg:gap-8">
-                    <p>DAYLENE.DESIGN@gmail.com</p>
-                    <p>BLESSANABLE MAQDAYLENE</p>
-                </div>
+        <div
+            class="text-white py-8 bg-conic-90 from-[#A1BDB8] via-[var(--color-terracotta)] to-[var(--color-light-pink)]">
+            <div class="flex flex-col lg:flex-row lg:justify-between px-8 gap-4 lg:gap-0 text-base">
+                <p>BLESSANABLE MAQDAYLENE</p>
                 <p>IG @DAYLENE.DESIGN</p>
+                <p>DAYLENE.DESIGN@gmail.com</p>
             </div>
         </div>
-        <p class="text-orange-500 text-center md:text-left">©2025 Daylene Maqdaylene. All rights reserved.</p>
+        <p class="pl-8 text-terracotta text-sm text-center md:text-left">©2025 Daylene Maqdaylene. All rights reserved.</p>
     </div>
 </template>
